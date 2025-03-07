3f68b6b2426ebc86419bc90429eae637f07f422a
Eddio0141 <eddio0141@gmail.com>
2025-03-07 21:52:46 +0000

    setup dev env
    ignore .direnv

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    {
+      flake-parts,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem =
+        {
+          system,
+          pkgs,
+          ...
+        }:
+        let
+          rust = pkgs.rust-bin.selectLatestNightlyWith (t: t.default);
+
+          # the correct rust-doc command is made, no need to modify this
+          rust-doc = pkgs.writeShellApplication {
+            name = "rust-doc";
+            text = ''
+              xdg-open "${rust}/share/doc/rust/html/index.html"
+            '';
+          };
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (import rust-overlay)
+            ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              (rust.override {
+                extensions = [
+                  "rust-analyzer"
+                  "rust-src"
+                ];
+              })
+              rust-doc
+            ];
+          };
+        };
+    };
+}
